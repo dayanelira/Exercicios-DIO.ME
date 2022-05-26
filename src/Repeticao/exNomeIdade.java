4863470ecbe9115f7d537771a7e580616348211e
Dayane Lira <dayanexlira@gmail.com>
2022-05-26 00:22:35 -0300

    Initial commit

---
+++ src/Repeticao/exNomeIdade.java
@@ -0,0 +1,22 @@
+package Repeticao;
+
+import java.util.Scanner;
+
+public class exNomeIdade {
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+
+        String nome;
+        int idade;
+
+        while(true) {
+            System.out.println("Nome:");
+            nome = scan.next();
+            if (nome.equals("0")) break;
+
+            System.out.println("Idade:");
+            idade = scan.nextInt();
+        }
+        System.out.println("Termina aqui!");
+    }
+}
